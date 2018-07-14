ed6dafce2567a3be324c4716db352d7dd64f0348
aman9463 <aman9463@gmail.com>
2018-07-14 22:44:57 +0530

    users and session controller generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  get 'dashboard/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+    root 'dashboard#show'
+
+  get 'users/new'
+  get 'login', to: 'sessions#new'
+  get 'logout', to: 'sessions#destroy'
+
+  resources :sessions, only: [:create]
+  resources :users, only: [:new, :create]
 end
